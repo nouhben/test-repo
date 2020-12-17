6d72a4d940aeb7c06bf8dbd2fad9b1e23f8b09fd
nouh ben kadi <benkadi.nouh@icloud.com>
2020-12-17 10:59:10 +0100

    merge

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -120,3 +120,19 @@ STATIC_URL = '/static/'
 #         'rest_framework.permissions.IsAuthenticated',
 #     ]
 # }
+
+# REST_FRAMEWORK = {
+#     # Use Django's standard `django.contrib.auth` permissions,
+#     # or allow read-only access for unauthenticated users.
+#     'DEFAULT_PERMISSION_CLASSES': [
+#         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+#     ]
+# }
+# REST_FRAMEWORK = {
+#     'DEFAULT_AUTHENTICATION_CLASSES': [
+#         'rest_framework.authentication.TokenAuthentication',
+#     ],
+#     'DEFAULT_PERMISSION_CLASSES': [
+#         'rest_framework.permissions.IsAuthenticated',
+#     ]
+# }
